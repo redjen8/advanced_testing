6c26fefcc5dcd26cdc94e58764a943a228af358c
sw-jeong <sw.jeong@navercorp.com>
2024-05-15 15:37:46 +0900

    Feat: 프로젝트 최초 세팅

---
+++ advanced_testing/src/main/java/com/example/advanced_testing/AdvancedTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanced_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedTestingApplication.class, args);
+    }
+
+}
---
+++ advanced_testing/src/test/java/com/example/advanced_testing/AdvancedTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advanced_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
