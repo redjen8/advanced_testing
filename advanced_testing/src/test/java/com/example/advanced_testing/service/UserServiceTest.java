4d11abee79761940d241de8cf84b0fe058bc38c4
sw-jeong <sw.jeong@navercorp.com>
2024-05-31 22:15:47 +0900

    Feat: fixture monkey 테스트 케이스 추가

--- advanced_testing/src/test/java/com/example/advanced_testing/service/UserServiceTest.java
+++ advanced_testing/src/test/java/com/example/advanced_testing/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.advanced_testing.service;
 
+import static org.assertj.core.api.BDDAssertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest
@@ -36,12 +41,29 @@ public class UserServiceTest {
 
     @ParameterizedTest
     @AutoSource
-    void 사용자_가입_테스트(String name, @Min(20) @Max(99) int age) {
+    void 사용자_가입_테스트_autoParams(String name, @Min(20) @Max(99) int age) {
         StepVerifier.create(userService.registerUser(name, age))
             .expectNextMatches(user -> user.getAge() == age &&
                 user.getName().equals(name) &&
                 UserStatus.ACTIVE.equals(user.getStatus()))
             .verifyComplete();
+    }
+
+    @Test
+    void 사용자_가입_테스트_fixtureMonkey() {
+        FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+            .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+            .build();
+
+        final Integer age = 26;
+
+        User user = fixtureMonkey.giveMeBuilder(User.class)
+            .set("age", 26)
+            .sample();
+
+        System.out.println(user);
+
+        then(user.getAge()).isEqualTo(26);
     }
 
 }
